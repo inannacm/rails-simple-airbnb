d34517c2423539cac52cedcb4996127f478df527
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-05-13 21:33:55 +0100

    airbnb rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+  flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Spacious Flat London',
+    address: '10 Strand London W9 1DT',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptatibus, ipsum sequi dolores incidunt totam sint possimus vero odio at quod nemo deserunt similique aperiam illum cum excepturi, quisquam. Quos!',
+    price_per_night: 60,
+    number_of_guests: 2
+  },
+  {
+    name: 'Small Flat',
+    address: '11 kingsland London W9 1DT',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ',
+    price_per_night: 75,
+    number_of_guests: 4
+  },
+  {
+    name: 'Central City Apartment',
+    address: '11 Gainsford St',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 100,
+    number_of_guests: 5
+  },
+  {
+    name: 'Convenient Flat with Large View',
+    address: 'Aldwych St',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In voluptatibus.',
+    price_per_night: 70,
+    number_of_guests: 2
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
